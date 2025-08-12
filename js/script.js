8b184c5cea5d3dad818dd8462f41c123bcd4d9cc
EvelinFerreiraAraujo <144921034+EvelinFerreiraAraujo@users.noreply.github.com>
2025-08-12 15:55:49 -0300

    salvar

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
+const botaoIniciar = document.querySelector(".iniciar-btn");
+const telaInicial = document.querySelector(".tela-inicial");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+
+let atual = 0;
+let historiaFinal = "";
+
+botaoIniciar.addEventListener('click', iniciaJogo);
+
+function iniciaJogo() {
+    atual = 0;
+    historiaFinal = "";
+    telaInicial.style.display = 'none';
+    caixaPerguntas.classList.remove("mostrar");
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultado.classList.remove("mostrar");
+    mostraPergunta();
+}
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+}
+
+function substituiNome() {
+}
+
+function mostraResultado() {
+    caixaResultado.classList.add("mostrar");
+    textoResultado.textContent = historiaFinal;
+}
